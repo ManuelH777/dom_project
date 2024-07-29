6e5405887a5e574ef85c732c3f4b60e3d6debec7
ManuelH777 <manuelkhuerta@gmail.com>
2024-07-29 15:35:59 -0400

    added changes

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Add your code to this file
+document.body.style.backgroundColor = "#00008B";
+let line = document.querySelector("h2");
+line.innerText = "Here are Pokemon's incredible Gen 1 starters!";
+line.style.textAlign = "center";
+
+let pokeImg = document.getElementById("starters_image_div");
+pokeImg.style.borderStyle = "dotted";
+pokeImg.style.borderColor = "orange";
+pokeImg.style.marginLeft = "710px";
+pokeImg.style.marginRight = "600px";
+
+let poke = document.getElementsByClassName("pokemon_images");
+poke.style.borderStyle = "solid";
+
+
